9344bb1099fb1f4741679b2c96a1e8e90f3e4892
szkatalin <szkatalin@outlook.com>
2020-08-06 14:02:12 +0200

    Updates

---
+++ precache-manifest.3770b5944575b490132d699e127cfcc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d29c3acf4bc8d9d7b894e69f255716c7",
+    "url": "/react-roll-dice/index.html"
+  },
+  {
+    "revision": "8b32d9c836b0d29093c3",
+    "url": "/react-roll-dice/static/css/main.a08569e5.chunk.css"
+  },
+  {
+    "revision": "e0daabb0d557ab4859e5",
+    "url": "/react-roll-dice/static/js/2.fb2976a5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-roll-dice/static/js/2.fb2976a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b32d9c836b0d29093c3",
+    "url": "/react-roll-dice/static/js/main.0030cc1e.chunk.js"
+  },
+  {
+    "revision": "a46270a7dae093a33049",
+    "url": "/react-roll-dice/static/js/runtime-main.34d67527.js"
+  }
+]);
